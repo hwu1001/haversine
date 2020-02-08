31cd3d1f501d500e56e0a9923820b0ce6d50dfef
Henry Wu <hywu925@gmail.com>
2020-02-08 10:58:00 -0800

    Add fn implementing Haversine formula

---
+++ haversine.zig
@@ -0,0 +1,62 @@
+const std = @import("std");
+const math = std.math;
+const testing = std.testing;
+
+pub const InvalidCoordinate = error {
+    InvalidLatitude,
+    InvalidLongitude,
+};
+
+// Mean Earth radius - https://en.wikipedia.org/wiki/Earth_radius#Mean_radius
+// Estimation by International Union of Geodesy and Geophysics - this value
+// is apparently not agreed upon globally per 
+// https://nssdc.gsfc.nasa.gov/planetary/factsheet/fact_notes.html
+const meanEarthRadius = 6371.0088;
+
+pub const InvalidRadius = error.InvalidRadius;
+
+pub const LatLng = struct {
+    lat: f64,
+    lng: f64,
+
+    pub fn validate(self: *const LatLng) InvalidCoordinate!void {
+        if (self.lat < -90.0 or self.lat > 90.0) {
+            return InvalidCoordinate.InvalidLatitude;
+        }
+        if (self.lng < -180.0 or self.lng > 180) {
+            return InvalidCoordinate.InvalidLongitude;
+        }
+        
+        return;
+    }
+};
+
+fn degreesToRadians(degrees: f64) f64 {
+    return degrees * math.pi / 180.0;
+}
+
+pub fn calculateDistance(coord1: LatLng, coord2: LatLng, radius: f64) !f64 {
+    if (radius <= 0.0) {
+        return InvalidRadius;
+    }
+    try coord1.validate();
+    try coord2.validate();
+
+    const lat1 = degreesToRadians(coord1.lat);
+    const lng1 = degreesToRadians(coord1.lng);
+    const lat2 = degreesToRadians(coord2.lat);
+    const lng2 = degreesToRadians(coord2.lng);
+
+    const diffLat = lat2 - lat1;
+    const diffLng = lng2 - lng1;
+
+    // the square of half the chord length between the points
+    const a = math.pow(f64, math.sin(diffLat / 2.0), 2.0) +
+              math.cos(lat1) * math.cos(lat2) *
+              math.pow(f64, math.sin(diffLng / 2.0), 2.0);
+    return 2 * radius * math.asin(math.sqrt(a));
+}
+
+pub fn calculateEarthDistance(coord1: LatLng, coord2: LatLng) !f64 {
+    return try calculateDistance(coord1, coord2, meanEarthRadius);
+}
